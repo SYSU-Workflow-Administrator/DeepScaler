efc2632898cbb58d2f724f7d7db73b4e6fea106f
宋世杰 <48445199+ssJokerss@users.noreply.github.com>
2023-08-14 10:23:27 +0800

    Update load_generator.py

--- sendFlow/load_generator.py
+++ sendFlow/load_generator.py
@@ -150,12 +150,12 @@ class StagesShape(LoadTestShape):
     def __init__(self):
         super().__init__()
         lines = []
-        with open("/ssj/ssj/boutiquessj/pyboutique/sendflow/random-100max.req", 'r') as f:
+        with open("/sendflow/random-100max.req", 'r') as f:
         #with open("/home/ssj/boutiquessj/pyboutique/sendflow/normalFlow.req", 'r') as f:
             lines = list(map(int, f.readlines()))
             lines = [x for i,x in enumerate(lines) if i%1==0]
-            self.lines = ([1]*5+lines+[1]*5)#又给了几个波谷
-            #self.lines = lines#又给了几个波谷
+            self.lines = ([1]*5+lines+[1]*5)
+            #self.lines = lines
     
     def tick(self):
         run_time = self.get_run_time()
@@ -164,8 +164,8 @@ class StagesShape(LoadTestShape):
         #while True:
         for _ in range(10):#
             for i, v in enumerate(self.lines):
-                #if run_time < (i+1)*10:#间隔为10s
-                if run_time < (i+1)*5:#间隔为10s
+                #if run_time < (i+1)*10:#internal 10s
+                if run_time < (i+1)*5:#internal为5
                     tick_data = (v, 100)                
             # user_count -- Total user count
             # spawn_rate -- Number of users to start/stop per second when changing number of users
